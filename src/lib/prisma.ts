b3220440fc4ac0a8552cb386645e97a12a6ef156
camlami <camilakerps@hotmail.com>
2026-01-31 17:40:20 -0300

    feat: configuraciones prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@/generated/prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+    // Opcional: útil para debugear en la consola de la terminal
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
