d88b86c1b78561c3b5e6ff74a1e52bc23e6642b0
ckerps <camilakerps@hotmail.com>
2026-04-12 23:47:56 -0300

    fix: pwa configuraciones

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-const publicRoutes = ['/login', '/logout'];
+const publicRoutes = ['/login', '/logout', '/manifest.webmanifest'];
 const adminOnlyRoutes = ['/usuarios'];
 const vendorExcludedRoutes = ['/ofertas', '/usuarios'];
 
